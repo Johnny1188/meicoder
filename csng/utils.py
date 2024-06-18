70a6de957ce5c38d7f12e1c1760acedf69f0cb4a
Jan Sobotka <jsobotka1188@gmail.com>
2024-06-18 21:27:56 +0000

    add slugify to utils

--- csng/utils.py
+++ csng/utils.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 import numpy as np
 import matplotlib.pyplot as plt
+import unicodedata
+import re
 
 
 def get_corr(x, y):
@@ -347,3 +349,20 @@ def dict_to_str(d, as_filename=False):
         return "___".join([f"{k}={print_val(v)}" for k, v in d.items()])
     else:
         return ", ".join([f"{k}: {print_val(v)}" for k, v in d.items()])
+
+
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
