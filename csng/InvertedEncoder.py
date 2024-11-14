cc196123e020209ce235e97c090ac85753245730
Jan Sobotka <jsobotka1188@gmail.com>
2024-11-14 01:01:35 +0100

    remove comments

--- csng/InvertedEncoder.py
+++ csng/InvertedEncoder.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-from scipy import signal
 import dill
 import torch
 import torch.nn.functional as F
@@ -193,63 +192,3 @@ class InvertedEncoderBrainreader(nn.Module):
         recons.append(recon.detach())
 
         return torch.cat(recons, dim=0)
-
-
-# class GaussianBlur:
-#     """
-#     Source: https://github.com/sinzlab/energy-guided-diffusion/blob/main/scripts/reconstruct_gd.py
-#     """
-
-#     """Blur an image with a Gaussian window.
-#     Arguments:
-#         sigma (float or tuple): Standard deviation in y, x used for the gaussian blurring.
-#         decay_factor (float): Compute sigma every iteration as `sigma + decay_factor *
-#             (iteration - 1)`. Ignored if None.
-#         truncate (float): Gaussian window is truncated after this number of standard
-#             deviations to each side. Size of kernel = 8 * sigma + 1
-#         pad_mode (string): Mode for the padding used for the blurring. Valid values are:
-#             'constant', 'reflect' and 'replicate'
-#         mei_only (True/False): for transparent mei, if True, no Gaussian blur for transparent channel:
-#             default should be False (also for non transparent case)
-#     """
-
-#     def __init__(
-#         self, sigma, decay_factor=None, truncate=4, pad_mode="reflect",
-#     ):
-#         self.sigma = sigma if isinstance(sigma, tuple) else (sigma,) * 2
-#         self.decay_factor = decay_factor
-#         self.truncate = truncate
-#         self.pad_mode = pad_mode
-
-#     def __call__(self, x, iteration=None):
-#         ### update sigma if needed
-#         if self.decay_factor is None:
-#             sigma = self.sigma
-#         else:
-#             sigma = tuple(s + self.decay_factor * (iteration - 1) for s in self.sigma)
-
-#         ### define 1-d kernels to use for blurring
-#         y_halfsize = max(int(round(sigma[0] * self.truncate)), 1)
-#         y_gaussian = signal.gaussian(2 * y_halfsize + 1, std=sigma[0])
-#         x_halfsize = max(int(round(sigma[1] * self.truncate)), 1)
-#         x_gaussian = signal.gaussian(2 * x_halfsize + 1, std=sigma[1])
-#         y_gaussian = torch.as_tensor(y_gaussian, device=x.device, dtype=x.dtype)
-#         x_gaussian = torch.as_tensor(x_gaussian, device=x.device, dtype=x.dtype)
-
-#         ### blur
-#         c = x.shape[1]
-#         padded_x = F.pad(
-#             x,
-#             pad=(x_halfsize, x_halfsize, y_halfsize, y_halfsize),
-#             mode=self.pad_mode,
-#         )
-#         blurred_x = F.conv2d(
-#             padded_x,
-#             y_gaussian.repeat(c, 1, 1)[..., None],
-#             groups=c,
-#         )
-#         blurred_x = F.conv2d(
-#             blurred_x, x_gaussian.repeat(c, 1, 1, 1), groups=c
-#         )
-#         final_x = blurred_x / (y_gaussian.sum() * x_gaussian.sum())  # normalize
-#         return final_x
