4f25a5376d13af0863f93f215e2dfc177fb727aa
Mikulas Vanousek <vanousek@izar1.izar.cluster>
2024-12-05 11:43:40 +0100

    wip

--- run_imagenet_inversion.sh
+++ run_imagenet_inversion.sh
@@ -6,9 +6,10 @@
 #SBATCH --gres=gpu:volta:1
 #SBATCH --time=2:00:00
 
+# Record the start time
+START_TIME=$(date +%s)
 
 echo "Start: $(date)"
-nvidia-smi
 
 ### Activate conda environment
 source $HOME/miniconda3/bin/activate csng
@@ -21,4 +22,13 @@ export $(cat .env | xargs)
 # Using the SLURM job ID to create a unique filename for the output notebook
 jupyter nbconvert --to notebook --execute --output resnet_inversion_${SLURM_JOB_ID}.ipynb csng/brainreader_mouse/resnet_inversion.ipynb
 
+nvidia-smi
+
 echo "End: $(date)"
+
+# Record the end time and calculate duration
+END_TIME=$(date +%s)
+ELAPSED_TIME=$((END_TIME - START_TIME))
+
+# Format the elapsed time as HH:MM:SS
+printf "Job Duration: %02d:%02d:%02d\n" $((ELAPSED_TIME/3600)) $(((ELAPSED_TIME%3600)/60)) $((ELAPSED_TIME%60))
