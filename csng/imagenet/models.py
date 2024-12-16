63c070a8d9c4ff86ca87f38f627f45186659051c
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-12-16 16:24:14 +0100

    Automated commit at 12/16/2024 16:24

--- csng/imagenet/models.py
+++ csng/imagenet/models.py
@@ -143,3 +143,6 @@ class UpsampleModel(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
